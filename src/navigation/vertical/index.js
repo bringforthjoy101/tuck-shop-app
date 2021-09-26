3b0ca86f190f842a7299b4adb0ff450c84b32fcb
Emmanuel Adelugba <adelugba.emma@gmail.com>
2021-09-26 13:52:14 +0100

    ...

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -10,4 +10,4 @@ import transaction from './transaction'
 const userData = JSON.parse(localStorage.getItem('userData'))
 
 // ** Merge & Export
-export default userData.role === 'manager' ? [...dashboards, ...tuckShop, ...admins, ...students, ...products, ...orders, ...transaction] : userData.role === 'busary' ? [...dashboards, ...tuckShop, ...students, ...transaction] : [...tuckShop, ...students]
+export default userData?.role === 'manager' ? [...dashboards, ...tuckShop, ...admins, ...students, ...products, ...orders, ...transaction] : userData?.role === 'busary' ? [...dashboards, ...tuckShop, ...students, ...transaction] : [...tuckShop, ...students]
