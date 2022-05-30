812773e8eb5a8e5f982148e78f4ad2072841465e
Emmanuel Adelugba <adelugba.emma@gmail.com>
2022-05-30 13:44:34 +0100

    ...

--- src/@core/auth/jwt/jwtDefaultConfig.js
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -3,17 +3,17 @@ export const apiUrl = process.env.REACT_APP_API_ENDPOINT
 
 // ** Auth Endpoints
 export default {
-  loginEndpoint: '/jwt/login',
-  registerEndpoint: '/jwt/register',
-  refreshEndpoint: '/jwt/refresh-token',
-  logoutEndpoint: '/jwt/logout',
-  tuckShopLoginEndpoint: `${apiUrl}/login`,
+	loginEndpoint: '/jwt/login',
+	registerEndpoint: '/jwt/register',
+	refreshEndpoint: '/jwt/refresh-token',
+	logoutEndpoint: '/jwt/logout',
+	tuckShopLoginEndpoint: `${apiUrl}/login`,
 
-  // ** This will be prefixed in authorization header with token
-  // ? e.g. Authorization: Bearer <token>
-  tokenType: 'Bearer',
+	// ** This will be prefixed in authorization header with token
+	// ? e.g. Authorization: Bearer <token>
+	tokenType: 'Bearer',
 
-  // ** Value of this property will be used as key to store JWT token in storage
-  storageTokenKeyName: 'accessToken',
-  storageRefreshTokenKeyName: 'refreshToken'
+	// ** Value of this property will be used as key to store JWT token in storage
+	storageTokenKeyName: 'accessToken',
+	storageRefreshTokenKeyName: 'refreshToken',
 }
